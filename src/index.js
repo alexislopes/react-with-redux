d6086c0c6dca6003b20aed22a2cad5a9de28e997
alexislopes <alexisdrops@gmail.com>
2019-05-24 12:01:50 -0300

    some styling and set of variables

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-
 // create a react component
 const App = () => {
-    return <div>Hi there!</div>
+
+    const buttonText = {text: 'click me!'};
+    const labelText = 'Enter name: ';
+    return (
+        <div>
+            <label className="lable" htmlFor="name">{labelText} </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+        </div>
+    );
 }
 
 
